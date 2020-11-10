062922e8e300f9d5b4cb0380852133d08e0bc3b4
William Sinn <59967116+sinnwc@users.noreply.github.com>
2020-11-10 10:01:08 -0500

    updated app for browser

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import feathers from '@feathersjs/feathers'
+import rest from '@feathersjs/rest-client'
+import axios from 'axios'
+
+const api = feathers()
+const rc = rest('http://localhost:3030')
+api.configure(rc.axios(axios))
+
+export default api
