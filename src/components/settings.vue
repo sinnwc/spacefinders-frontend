819f29834d7d01b40f342be4268eaec394c7ad03
William Sinn <59967116+sinnwc@users.noreply.github.com>
2020-12-07 12:59:29 -0500

    added form

--- src/components/settings.vue
+++ src/components/settings.vue
@@ -1,11 +1,32 @@
-<template>
-    <div>
-        settings
-    </div>
-</template>
-
 <script>
+import ddForm from 'vue-dd-form'
 export default {
-  name: 'settings'
+  components: { ddForm },
+  data: () => ({
+    usernameset: {},
+    exampleUsernames: {
+      name: { view: 'text', label: 'Username' },
+      studentemail: { view: 'text', label: 'Student e-mail' },
+      classtimes: {
+        view: 'checkbox',
+        label: 'Check Your Class Time',
+        options: [
+          'MWF',
+          'TuThu',
+          'MWThuF',
+          'MWThu',
+          'TuWThur'
+        ]
+      }
+    }
+  })
 }
 </script>
+
+<template>
+  <dd-form
+    :data="usernameset"
+    :descriptions="exampleUsernames"
+    @submit="submit">
+  </dd-form>
+</template>
